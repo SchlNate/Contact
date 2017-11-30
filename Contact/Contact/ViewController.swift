76cbd72a70aea35f44ed9d8256006f75808343b5
Nathan Schlechte <nas4m7@mail.missouri.edu>
2017-11-29 19:15:51 -0600

    first commit

---
+++ Contact/Contact/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contact
+//
+//  Created by Nathan Schlechte on 11/29/17.
+//  Copyright © 2017 Nathan Schlechte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
